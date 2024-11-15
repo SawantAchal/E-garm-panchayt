07035a155ac417c6db337a06cd27611e8d81794a
SawantAchal <achals1718@gmail.com>
2024-11-15 20:51:48 +0530

    token state

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,15 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
     const url = 'http://localhost:4000'
+    const [token , setToken] = useState([])
 
     const contextValue = {
         url,
+        token,
+        setToken
     }
 
     return (
