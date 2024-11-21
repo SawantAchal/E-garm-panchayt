ea008a636414b23d3b9d255ab18e99cdf75750b1
SawantAchal <achals1718@gmail.com>
2024-11-22 00:58:26 +0530

    model for appliation

---
+++ backend/models/applicationModel.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const applicationSchema = new mongoose.Schema({
+  userId: { type:String,required:true },
+  serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'service', required: true },
+  serviceName: { type: String },
+  fullName: { type: String, required: true },
+  address: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  // documents: { type: String },
+  // status: { type: String, default: 'Pending' }, // Application status
+}, { timestamps: true });
+
+const applicationModel = mongoose.models.application || mongoose.model('application', applicationSchema);
+
+export default applicationModel;
