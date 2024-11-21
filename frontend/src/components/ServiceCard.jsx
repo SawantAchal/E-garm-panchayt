8c4e1709e05fe4dccbbf7e9bcb4e8ed2764834fe
SawantAchal <achals1718@gmail.com>
2024-11-22 01:30:01 +0530

    add service is

--- frontend/src/components/ServiceCard.jsx
+++ frontend/src/components/ServiceCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const ServiceCard = ({ service }) => {
+
+  const navigate = useNavigate()
+  const handleApply = () => {
+    navigate('/appliForm', { state: { serviceName: service.name ,serviceId: service._id,} })
+  }
+
   return (
     <>
-        <div className="border rounded-lg p-4 bg-white shadow-md hover:shadow-lg">
-            <h3 className="font-semibold">{service.name}</h3>
-            <p>{service.description}</p>
-            <button className="bg-blue-500 text-white px-4 py-2 mt-2 rounded">Apply</button>
-        </div>
+      <div className="border rounded-lg p-4 bg-white shadow-md hover:shadow-lg">
+        <h3 className="font-semibold">{service.name}</h3>
+        <p>{service.description}</p>
+        <button onClick={handleApply} className="bg-blue-500 text-white px-4 py-2 mt-2 rounded">Apply</button>
+      </div>
     </>
   )
 }
