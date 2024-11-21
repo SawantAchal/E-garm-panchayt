794c14daf13a568dfe8a657cf11e2c848782af81
SawantAchal <achals1718@gmail.com>
2024-11-22 01:30:28 +0530

    add protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,22 +6,33 @@ import Sidebar from './components/Sidebar'
 import ApplicationForm from './components/ApplicationForm'
 import ApplicationStatus from './components/ApplicationStatus'
 import Profile from './components/Profile'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react'
+
+const ProtectedRoute = ({ children, isAuthenticated }) => {
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem('gram_panchayt')
+  );
 
   return (
     <>
-      <Navbar/>
-      <Login/>
-      <div className='flex pt-16'>
-        <Sidebar className="w-1/4 min-h-screen "/>
-        <div className='flex-1 ml-64'>
+      <ToastContainer/>
+      <Navbar setIsAuthenticated={setIsAuthenticated}/>
+      <div className={`flex pt-16 ${!isAuthenticated && 'justify-center'}`}>
+      {isAuthenticated && <Sidebar className="w-1/4 min-h-screen" />}
+        <div className={`flex-1 ${isAuthenticated ? 'ml-64' : ''}`}>
           <Routes>
-            <Route path='/allServices' element={<AllServices/>}/>
-            <Route path='/applicationStatus' element={<ApplicationStatus/>}/>
-            <Route path='/profile' element={<Profile/>}/>
-            <Route path='/appliForm' element={<ApplicationForm/>}/>
+            <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />}/>
+            <Route path='/allServices' element={<ProtectedRoute isAuthenticated={isAuthenticated}><AllServices/></ProtectedRoute>}/>
+            <Route path='/applicationStatus' element={<ProtectedRoute isAuthenticated={isAuthenticated}><ApplicationStatus/></ProtectedRoute>}/>
+            <Route path='/profile' element={<ProtectedRoute isAuthenticated={isAuthenticated}><Profile/></ProtectedRoute>}/>
+            <Route path='/appliForm' element={<ProtectedRoute isAuthenticated={isAuthenticated}><ApplicationForm/></ProtectedRoute>}/>
           </Routes>
         </div>
       </div>
