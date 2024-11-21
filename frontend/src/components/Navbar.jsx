e8b404bba40967087354da006b4c91024a305f25
SawantAchal <achals1718@gmail.com>
2024-11-22 01:28:56 +0530

    add logout functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,21 +2,22 @@ import React, { useContext } from 'react'
 import { StoreContext } from '../context/storeContext'
 import { useNavigate } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setIsAuthenticated}) => {
   const {setToken} = useContext(StoreContext)
   const navigate = useNavigate()
 
   const logout = () => {
-    localStorage.removeItem("gram panchayt")
+    localStorage.removeItem("gram_panchayt")
+    setIsAuthenticated(false)
     setToken("")
-    // navigate('/')
+    navigate('/')
   }
 
   return (
     <>
       <nav className="bg-gray-800 text-white p-4 fixed w-full h-16 top-0 left-0">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
-          <div className="text-xl font-bold">E-Gram Panchayat</div>
+          <div className="text-xl font-bold" onClick={() => navigate("/allServices")}>E-Gram Panchayat</div>
           <button onClick={logout} className="bg-blue-500 px-4 py-2 rounded">Logout</button>
         </div>
       </nav>
