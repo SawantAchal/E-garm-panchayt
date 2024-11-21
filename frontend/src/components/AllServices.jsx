9c9630b9d4e013ae06e8ed9edd2bd20164733e54
SawantAchal <achals1718@gmail.com>
2024-11-22 01:16:19 +0530

    display all services

--- frontend/src/components/AllServices.jsx
+++ frontend/src/components/AllServices.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ServiceCard from './ServiceCard';
+import { StoreContext } from '../context/storeContext'
+import axios from 'axios'
+import { toast } from 'react-toastify';
 
 const AllServices = () => {
-    const services = [
-        { id: 1, name: 'Apply for Water Supply', description: 'Get water supply connection.' },
-        { id: 2, name: 'Road Construction', description: 'Apply for road construction in your area.' },
-        { id: 3, name: 'Road Construction', description: 'Apply for road construction in your area.' },
-        { id: 4, name: 'Road Construction', description: 'Apply for road construction in your area.' },
-        { id: 5, name: 'Road Construction', description: 'Apply for road construction in your area.' }
-      ];
+    const {url} = useContext(StoreContext)
+    const [services , setServices] = useState([])
+
+    const fetchAllServices = async () => {
+        try {
+            const response = await axios.get(url+'/api/service/allServices')
+            // console.log(response.data.data)
+            if (response.data.success) {
+                setServices(response.data.data)
+            } else {
+                console.error("Failed to fetch services");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error({ success: false, message: error.message });
+        }
+    }
+
+    useEffect(() => {
+        fetchAllServices()
+    },[])
+
   return (
     <>
         <div>
@@ -16,12 +34,11 @@ const AllServices = () => {
                 <h2 className="text-2xl font-bold mb-4">Available Services</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {
-                        services.map((service) => (
-                            <ServiceCard key={service.id} service={service} />
+                        services.map((service , i) => (
+                            <ServiceCard key={i} service={service} />
                         ))
                     }
                 </div>
-
             </div>
         </div>
     </>
