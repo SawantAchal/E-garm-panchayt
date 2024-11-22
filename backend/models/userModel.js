1a22f7f2026e0ed8ebc9ef36c37b887eaf072682
SawantAchal <achals1718@gmail.com>
2024-11-23 01:17:21 +0530

    add status

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,13 +1,23 @@
-// create user model 
+
 import mongoose from 'mongoose'
 
-// user Schema
 const userSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-},{minimize:false})
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    applicationCart: [
+        {
+            serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'service' },
+            serviceName: { type: String },
+            fullName: { type: String },
+            address: { type: String },
+            phoneNumber: { type: String },
+            // documents: { type: String },
+            status: { type: String, default: 'Pending' },
+            date: { type: Date, default: Date.now },
+        }
+    ],
+}, { minimize: false , timestamps:true });
 
-// create model in database
-const userModel = mongoose.model.user || mongoose.model("user" ,userSchema)
+const userModel = mongoose.model.user || mongoose.model("user", userSchema);
 export default userModel;
