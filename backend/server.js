cbc81738562264c6a1ca5022d797f2b318ba64fb
SawantAchal <achals1718@gmail.com>
2024-11-23 01:26:34 +0530

    created a login for admin

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,11 @@ import 'dotenv/config'
 import { connectDB } from './config/db.js';
 import userRouter from './routes/userRoute.js';
 import serviceRouter from './routes/serviceRoute.js';
+import adminRouter from './routes/adminRoutes.js';
+import adminModel from './models/adminModel.js';
+import bcrypt from 'bcryptjs';
+import staffRouter from './routes/staffRoute.js';
+import applicationRouter from './routes/applicationRoutes.js';
 
 //app config
 const app = express();
@@ -16,9 +21,44 @@ app.use(cors());
 //db connect
 connectDB();
 
+// Check and Create Default Admin
+const createDefaultAdmin = async () => {
+  try {
+    const adminEmail = process.env.ADMIN_EMAIL;
+    const adminPassword = process.env.ADMIN_PASSWORD;
+
+    if (!adminEmail || !adminPassword) {
+      console.error('Admin credentials are missing in .env file');
+      return;
+    }
+
+    const existingAdmin = await adminModel.findOne({ email: adminEmail });
+    if (!existingAdmin) {
+      const hashedPassword = await bcrypt.hash(adminPassword, 10);
+      const admin = new adminModel({
+        name: 'Default Admin',
+        email: adminEmail,
+        password: hashedPassword,
+      });
+      await admin.save();
+      console.log('Default admin created successfully!');
+    } else {
+      console.log('Admin already exists.');
+    }
+  } catch (error) {
+    console.error('Error creating default admin:', error);
+  }
+};
+
+// Call the function on server startup
+createDefaultAdmin();
+
 //Routes
 app.use('/api/user' , userRouter)
 app.use('/api/service' , serviceRouter)
+app.use('/api/admin' , adminRouter)
+app.use('/api/staff' , staffRouter)
+app.use('/api/application' , applicationRouter)
 
 // request the data for server
 app.get('/' , (req , res) => {
