4d2c20e1cbcacb3067b4715ae74d10828e6760ce
SawantAchal <achals1718@gmail.com>
2024-11-23 01:18:22 +0530

    add route for application status

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authMiddleware from '../middleware/auth.js';
-import {  applyForService, getAllApplications } from '../controllers/applicationController.js';
+import {  applyForService, getAllApplications, updateStatus } from '../controllers/applicationController.js';
+import { adminAuth } from '../middleware/adminAuth.js';
 
 const applicationRouter = express.Router()
 
 applicationRouter.post('/apply' , authMiddleware , applyForService)
 applicationRouter.get('/allApplication' , getAllApplications)
+applicationRouter.post('/applicationStatus' , updateStatus)
 
 export default applicationRouter;
